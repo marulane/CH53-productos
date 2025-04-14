0271ba9600172267a0eb8e5fdf95b94e1cb5af42
Luisa <luisa.montesrmz@gmail.com>
2025-04-14 11:20:09 -0600

    Comentarios para facilitar lectura de código

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ const modalPrice = document.getElementById("modalPrice");
 const closeModalBtn = document.getElementById("closeModalBtn");
 
 
-const ulMenu = document.getElementById("ulMenu");
+const ulMenu = document.getElementById("ulMenu"); //lista de categorías (dropdown o menú lateral)
 
-const URLMain = "https://fakestoreapi.com/products/"; //debe llevar diagonal al final para facilitar escritura de futuros parametros agregados
+const URLMain = "https://fakestoreapi.com/products/"; //debe llevar diagonal al final para facilitar escritura de subrutas
 //La URL regresa un array de objetos
 function getData(cat){
     productgrid.innerHTML = ""; // Limpia productos anteriores
     //const options ={"method": "GET"};
-    fetch(URLMain+cat)//options)
+    fetch(URLMain+cat)//options) 
     .then((response)=> response.json())
     .then((data)=> {
         data.forEach((e) => {
@@ -27,7 +27,7 @@ function getData(cat){
                         <h5 class="card-title">${e.title}</h5>
                         <p class="card-text">${e.description.slice(0,100)}</p>
                         <div class="d-flex justify-content-center">
-                            <a href="#" class="btn btn-primary btn-detalles"
+                            <a href="#" class="btn btn-primary btn-detalles" 
                             data-title="${e.title}"
                             data-description ="${e.description}"
                             data-category ="${e.category}"
@@ -40,13 +40,18 @@ function getData(cat){
         });
 
         //Funcionalidad Modal
+        //Constante que almacena todos los elementos con la clase .btn-detalles (uno para cada tarjeta de producto creada)
         const botonesDetalles = document.querySelectorAll(".btn-detalles");
         botonesDetalles.forEach((btn) => {
+            //Agrega un event listener a cada botón
             btn.addEventListener("click", function(event){
                 event.preventDefault(); 
+                //Revela al modal
                 customModal.style.display = "block";
-                modalTitle.innerHTML= `<p>${btn.dataset.title}</p>`;
-                modalBody.innerHTML= `<p>${btn.dataset.description}</p>
+                //Lee y muestra los atributos data que se almacenaron de cada botón 
+                //Nombre de producto, descripción, precio y categoría
+                modalTitle.innerHTML= `<p>${btn.dataset.title}</p>`; 
+                modalBody.innerHTML= `<p>${btn.dataset.description}</p> 
                                         <span class="badge text-bg-info">Price: $${btn.dataset.price}</span>`;
                 modalFooter.innerHTML= `<strong><mark>Category: ${btn.dataset.category}</mark></strong>`;
             });
@@ -67,18 +72,27 @@ function getData(cat){
     });
 }//getData
 
+//Cerrando modal
+closeModalBtn.addEventListener("click", ()=>{
+    customModal.style.display = "none";
+})
 
+//Función para obtener las categorías desde la API
 function getCategories(){
     const options ={"method": "GET"};
     fetch(URLMain+"categories/", options)
+    //Obtiene los nombres de las categorías
     .then((response) =>{
         //console.log(response);
         response.json().then((res)=>{
             //console.log("categories:", res);
             res.forEach((cat)=>{
+                //Agregamos la categoría obtenida a la lista del HTML después de su apertura
                 ulMenu.insertAdjacentHTML("afterbegin",
                     `<li><a class="dropdown-item" style="cursor:pointer;" onclick="getData('category/${(cat.replace("'","%27"))}');">${cat}</a></li>`);   //No usar el replace porque no es una solucion global, solo particular   
             });
+                    //Insertamos a cada categoría un el evento onclick que ejecuta getData pasando como parámetro la subruta 'category' + nombre de la categoría obtenida en la iteración del forEach
+                    //Ejemplo de ruta: /category/electronics
         });
     })
     .catch((err) => {
@@ -89,11 +103,7 @@ function getCategories(){
     });
 }
 
-closeModalBtn.addEventListener("click", ()=>{
-    customModal.style.display = "none";
-})
-
 getCategories();
-getData("");
+getData(""); //Por defecto debe haber un parámetro así que lo pasamos como cadena vacía para que funcione
 
 
